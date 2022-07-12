277cbefa5117e5dff45ff4bc9377b919c6b5b5b0
Daniel Kolomeetz <kolomeetz.dany@yandex.ru>
2022-07-12 22:07:51 +0300

    Add handler to return all the albums

--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
-// TODO: take out from the main package
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
 // album represents data about a record album.
 type album struct {
 	ID     string  `json:"id"`
@@ -9,10 +14,24 @@ type album struct {
 	Price  float64 `json:"price"`
 }
 
-// TODO: hide under init() somewhere
 // albums slice to seed record album data.
 var albums = []album{
 	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
 	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
 	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
+
+func main() {
+	// Set up an association in which getAlbums handles requests
+	// to the /albums endpoint
+	router := gin.Default()
+	router.GET("/albums", getAlbums)
+
+	router.Run("localhost:8080")
+}
+
+// getAlbums responds with the list of all albums as JSON.
+func getAlbums(c *gin.Context) {
+	// https://pkg.go.dev/github.com/gin-gonic/gin#Context.JSON
+	c.JSON(http.StatusOK, albums)
 }
